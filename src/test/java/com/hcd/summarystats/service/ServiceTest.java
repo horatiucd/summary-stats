c007e90116d6f4ffc240b370428289de75ca34f5
horatiu.dan <horatiu.dan@tangoe.com>
2023-06-12 17:06:35 +0300

    Finalizes the implementation.

--- src/test/java/com/hcd/summarystats/service/ServiceTest.java
+++ src/test/java/com/hcd/summarystats/service/ServiceTest.java
@@ -26,36 +26,39 @@ abstract class ServiceTest {
         service = setupService();
 
         mothers = IntStream.rangeClosed(1, 3)
-                .mapToObj(i -> new Parent("Mother" + i, i + 10))
+                .mapToObj(i -> new Parent("Mother" + i, i + 30))
                 .collect(Collectors.toList());
 
-        fathers = IntStream.rangeClosed(4, 5)
-                .mapToObj(i -> new Parent("Father" + i, i + 10))
+        fathers = IntStream.rangeClosed(4, 6)
+                .mapToObj(i -> new Parent("Father" + i, i + 30))
                 .collect(Collectors.toList());
 
         parents = new ArrayList<>(mothers);
         parents.addAll(fathers);
     }
 
-    private void assertHumanStats(ParentStats stats) {
+    private void assertParentStats(ParentStats stats) {
         Assertions.assertNotNull(stats);
-        Assertions.assertEquals(5, stats.count());
-        Assertions.assertEquals(11, stats.youngest());
-        Assertions.assertEquals(15, stats.oldest());
-        Assertions.assertEquals(4, stats.ageRange());
-        Assertions.assertEquals((double) 65/5, stats.averageAge());
-        Assertions.assertEquals(11 + 12 + 13 + 14 + 15, stats.totalYearsOfAge());
+        Assertions.assertEquals(6, stats.count());
+        Assertions.assertEquals(31, stats.youngest());
+        Assertions.assertEquals(36, stats.oldest());
+        Assertions.assertEquals(5, stats.ageRange());
+
+        final int sum = 31 + 32 + 33 + 34 + 35 + 36;
+
+        Assertions.assertEquals((double) sum/6, stats.averageAge());
+        Assertions.assertEquals(sum, stats.totalYearsOfAge());
     }
 
     @Test
     void getStats() {
         final ParentStats stats = service.getStats(parents);
-        assertHumanStats(stats);
+        assertParentStats(stats);
     }
 
     @Test
     void getCombinedStats() {
         final ParentStats stats = service.getCombinedStats(mothers, fathers);
-        assertHumanStats(stats);
+        assertParentStats(stats);
     }
 }
